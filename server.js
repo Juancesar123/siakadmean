0da68833b5d3ed17d449b5a4bf96505c638ba67b
Juancesar123 <juancesarandrianto@gmail.com>
2016-08-15 23:46:07 +0700

    update hari ini
    update hari ini

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require("express");
 var app = express();
 var http = require('http').Server(app);
+app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.use('/bootstrap',express.static(__dirname + '/bootstrap'));
+app.use(express.static(__dirname + '/views'));
+app.use('/public',express.static(__dirname + '/public'));
 http.listen(3000);

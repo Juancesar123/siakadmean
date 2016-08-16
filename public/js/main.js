fd6f921b00dbd158fc3bb270851c999c5a8081a9
Juancesar123 <juancesarandrianto@gmail.com>
2016-08-16 08:09:45 +0700

    update terbaru
    update terbaru

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,48 @@
-var app = angular.module("mainApp",[]);
+var mainApp = angular.module("mainApp", ['ngRoute','datatables','checklist-model',"ngResource"]);
+ mainApp.config(function($routeProvider) {
+    $routeProvider
+      .when('/dataguru', {
+        templateUrl:"dataguru",
+			  controller :"dataguru"
+	});
+ });
+ mainApp.factory('socket', ['$rootScope', function($rootScope) {
+   var socket = io.connect();
+
+   return {
+     on: function(eventName, callback){
+       socket.on(eventName, callback);
+     },
+     emit: function(eventName, data) {
+       socket.emit(eventName, data);
+     }
+   };
+ }]);
+mainApp.directive('fileModel', ['$parse', function ($parse) {
+            return {
+               restrict: 'A',
+               link: function(scope, element, attrs) {
+                  var model = $parse(attrs.fileModel);
+                  var modelSetter = model.assign;
+
+                  element.bind('change', function(){
+                     scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                     });
+                  });
+               }
+            };
+         }]);
+
+mainApp.controller("dataguru",function($http,$scope){
+  $scope.dtOptions = DTOptionsBuilder.newOptions()
+          .withDisplayLength(5)
+          .withOption('bLengthChange', false)
+          .withOption('autoWidth', false)
+          .withOption('scrollX', false);
+          $scope.getdata = function(){
+          $http.get("lihat_gedung").success(function(data){
+            $scope.gedung= data;
+          });
+      }
+})
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require("express");
 var app = express();
 var http = require('http').Server(app);
+var bodyParser=require("body-parser");
+var mongojs = require("mongojs");
+var db = mongojs("siakad",["dataguru"]);
 app.set('view engine', 'jade');
 app.set('view engine', 'ejs');
 app.use('/bootstrap',express.static(__dirname + '/bootstrap'));
 app.use(express.static(__dirname + '/views'));
 app.use('/public',express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.get("/dataguru",function(req,res){
+  res.render("dataguru.ejs");
+});
+app.get("/ambil_dataguru",function(req,res){
+  db.dataguru.find(function(err,docs){
+    res.json(docs)
+  })
+});
+
 http.listen(3000);
